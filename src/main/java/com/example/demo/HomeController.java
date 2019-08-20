f74101ae991965b8353ed20aec65194276921187
chulahn <scahn.x@gmail.com>
2019-08-20 14:53:49 -0400

    initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+//    @Autowired
+//    JobRepository jobRepository;
+//
+//    @RequestMapping("/")
+//    public String listJobs(Model model){
+//        model.addAttribute("jobs", jobRepository.findAll());
+//        return "list";
+//    }
+//
+//    @GetMapping("/add")
+//    public String jobForm(Model model) {
+//        model.addAttribute("job", new Job());
+//        return "jobform";
+//    }
+//
+//    @PostMapping("/process")
+//    public String processForm(@Valid Job job, BindingResult result){
+//        if (result.hasErrors()) {
+//            return "jobform";
+//        }
+//        jobRepository.save(job);
+//        return "redirect:/";
+//    }
+
+    @Autowired
+    TodoRepository todoRepository;
+
+    @RequestMapping("/")
+    public String listTodos(Model model){
+        model.addAttribute("todos", todoRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String jobForm(Model model) {
+        model.addAttribute("todo", new Todo());
+        return "todoform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Todo todo, BindingResult result){
+        if (result.hasErrors()) {
+            return "todoform";
+        }
+        todoRepository.save(todo);
+        return "redirect:/";
+    }
+}
